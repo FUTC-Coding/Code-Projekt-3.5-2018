7ffef125f1693b9d8007caeca411333d498d7587
Gerrit Holzbaur <gerrholz18@computercamp.local>
2018-08-23 17:44:06 +0200

    Finished Tree Class

--- src/src/at/computercamp/utttai/tree/Tree.java
+++ src/src/at/computercamp/utttai/tree/Tree.java
@@ -1,4 +1,29 @@
 package at.computercamp.utttai.tree;
 
 public class Tree {
+
+
+    Node root;
+
+    public Tree() {
+        root = new Node();
+    }
+
+    public Tree(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public void addChild(Node parent, Node child) {
+        parent.getChildren().add(child);
+    }
+
+
 }
