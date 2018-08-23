cb84d3dd33832232e59c3f985901b576db15d732
Tanonic <noprocraft@gmail.com>
2018-08-23 16:58:49 +0200

    finished UCT.java

--- src/src/at/computercamp/utttai/mcts/UCT.java
+++ src/src/at/computercamp/utttai/mcts/UCT.java
@@ -1,5 +1,7 @@
 package at.computercamp.utttai.mcts;
 
+import at.computercamp.utttai.tree.Node;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -11,5 +13,12 @@ public class UCT {
             return Integer.MAX_VALUE;
         }
         return(nodeWinScore / (double) nodeVisit) + 1.41 * Math.sqrt(Math.log(totalVisit) / (double) nodeVisit);
+    }
+
+    static Node findBestNodeWithUCT(Node node) {
+        int parentVisit = node.getState().getVisitCount();
+        return Collections.max(
+                node.getChildArray(),
+                Comparator.comparing(c -> uctValue(parentVisit, c.getState().getWinScore(), c.getState().getVisitCount())));
     }
 }
