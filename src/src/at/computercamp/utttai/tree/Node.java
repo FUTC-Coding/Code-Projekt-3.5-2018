b2f853ffe965fbe0ea95b6b23c0135e9a61143a4
Gerrit Holzbaur <gerrholz18@computercamp.local>
2018-08-23 16:42:59 +0200

    Changed Node Class

--- src/src/at/computercamp/utttai/tree/Node.java
+++ src/src/at/computercamp/utttai/tree/Node.java
@@ -1,14 +1,29 @@
 package at.computercamp.utttai.tree;
 
-import java.util.List;
+import at.computercamp.utttai.game.Board;
+import at.computercamp.utttai.game.State;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Node {
-    State state;
-    Node parent;
-    List<Node> childArray;
+
+    private ArrayList<Node> childs;
+    private Node parent;
+    private State state;
 
     public Node() {
-
-
+        state = new State();
+        childs = new ArrayList<>();
     }
+
+    public ArrayList<Node> getChilds() {
+        return childs;
+    }
+
+    public Node getRandomChild() {
+        Random random = new Random();
+        return childs.get(random.nextInt(childs.size()));
+    }
+
 }
